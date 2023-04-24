4539398add282eac7a458b0b486c52964f76949d
Gustavo T.S. ~gBit <g.tsantander.devarch@engineer.com>
2023-04-24 12:21:22 -0400

    Refactored index.js, work endpoints run script python on node. Enpoints run child_process: execFile, swarn. NOTE: Run api/v1/ on node 18.x

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-require("dotenv").config() // NOTE: for recognize files -> .env || .env.*
-const express = require('express')
+require("dotenv").config(); // NOTE: for recognize files -> .env || .env.*
+const express = require("express");
 // const { spawnSync } = require('child_process'); // TODO: PENDING test
-// const { spawn } = require('node:child_process'); // TODO: PENDING test
-const { execFile } = require("child_process");
+const {execFile, spawn} = require("child_process");
 
-const cors = require("cors")
-const app = express()
+const cors = require("cors");
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT_LOCAL || 3000;
 
-app.get('/wsc-ketal', (req, res) => {
+app.get("/scraping-ketal", (req, res) => {
+  /* eslint-disable-next-line max-len */
   const pythonFilePath = "./dist/scraping.ketal/scraping.ketal"; // EXEC GENERATED with PyINSTALLER from other _project Python
   execFile(pythonFilePath, (err, stdout, stderr) => {
     if (err) {
@@ -19,10 +19,45 @@ app.get('/wsc-ketal', (req, res) => {
       return;
     }
     // console.log('stdout >>>', stdout);
-    res.json(JSON.parse(stdout))
+    res.json(JSON.parse(stdout));
   });
-})
+});
+
+app.get("/wsc-ketal", (req, res) => {
+  const param = req.query.param;
+  console.log("PARAM", param);
+  const process = spawn("python3", ["wsc.ketal.py", param]);
+  process.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+    res.send(data);
+  });
+  process.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+    res.send(data);
+  });
+  process.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+});
+
+app.get("/my-endpoint", (req, res) => {
+  const param = req.query.someparam;
+  console.log("PARAM", param);
+
+  const process = spawn("python3", ["script.py", param]);
+  process.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+    res.send(data);
+  });
+  process.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+    res.send(data);
+  });
+  process.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port http://localhost:${port}`)
-})
+  console.log(`Listening on port http://localhost:${port}`);
+});
---
+++ script.py
@@ -0,0 +1,5 @@
+import sys
+
+param = sys.argv[1]
+
+print("Hola Mundo " + param)
