0faf0af241910899d7cf5816e03dcc9eb543b462
Gustavo T.S. ~gBit <g.tsantander.devarch@engineer.com>
2023-05-02 16:20:46 -0400

    Work API scraping e-commerce:FIDALGA. NOTE: diff use strip() for name_product

--- index.js
+++ index.js
@@ -75,6 +75,63 @@ app.get(apiPrefix + "/wsc-ketal", (req, res) => {
   });
 });
 
+app.get(apiPrefix + "/wsc-fidalga", (req, res) => {
+  // const param = req.query.param;
+  const paramUrl = req.query.requestUrl;
+  console.log("paramUrl.toString()>>>>>", paramUrl.toString());
+
+  const baseUrl = "https://www.fidalga.com";
+
+  // WIP: Validation
+  if (!paramUrl.toString().includes(baseUrl)) {
+    return res.status(422).json({
+      message: "Verifique que los datos sean válidos",
+      validation: [
+        {
+          field: `URL`, // requestUrl
+          message: `La url que envíe debe contener ${baseUrl}.`,
+        },
+      ],
+    });
+  }
+
+  // Array ["", "/"] => length = 1 || ["", "some-category"]  => length > 1
+  // Apply length to String
+  if (paramUrl.split(baseUrl)[1].length <= 1) {
+    return res.status(422).json({
+      message: "Verifique que los datos sean válidos",
+      validation: [
+        {
+          field: `URL`, // requestUrl
+          message: `La url que envíe debe ser de la página de búsqueda/filtrado (por ejemplo: ${baseUrl}/alguna-categoria). NO puede ser de la página principal, sin embargo, si desea agregar el soporte/funcionalidad de esta página, ¡contáctenos! `,
+        },
+      ],
+    });
+  }
+
+  // WIP: pythonProcess
+  const process = spawn("python3", ["wsc.fidalga.py", paramUrl]);
+  process.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+    return res.json(JSON.parse(data));
+  });
+  process.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+    return res.status(422).json({
+      message: "Verifique que los datos sean válidos",
+      validation: [
+        {
+          field: `URL`, // requestUrl
+          message: `${data}`,
+        },
+      ],
+    });
+  });
+  process.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+});
+
 app.get("/", (req, res) => {
   const param = req.query.someparam;
   console.log("PARAM", param);
